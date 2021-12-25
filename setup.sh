728b00f359a7cfe5a446c4b2cf292e1acaecb6e6
IbraHam-7198 <69617424+IbraHam-7198@users.noreply.github.com>
2021-12-25 21:23:33 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,10 @@
 mkdir -p ~/.streamlit/
 
 echo "[theme]
-base="light"
-primaryColor="#86BC24"
-secondaryBackgroundColor="#0F0808"
-textColor="#9c9da0"
-font = ‘sans serif’
+base='light'
+primaryColor='#86BC24'
+secondaryBackgroundColor='#0F0808'
+textColor='#9c9da0'
 [server]
 headless = true
 port = $PORT
